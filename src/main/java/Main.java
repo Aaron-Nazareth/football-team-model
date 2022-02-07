70204f5b6bdcd9d968155d2b10876a98f630b664
Aaron Nazareth <aaronnazareth@hotmail.co.uk>
2022-02-07 15:10:00 +0000

    add player method added to team service

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import teams.TeamService;
 public class Main {
 
     public static void main(String[] args) {
-        Player player1 = new Player("Mohammed Salah", "RW", 11, 5);
+        Player salah = new Player("Mohammed Salah", "RW", 11, 5);
 
         Manager klopp = new Manager("Jurgen Klopp");
 
@@ -15,7 +15,12 @@ public class Main {
 
         TeamService teamService = new TeamService();
 
-        int spacesInTeam = teamService.countEmptySpacesOnTeam(liverpool);
-        System.out.println(spacesInTeam);
+        int spacesInTeamBefore = teamService.countEmptySpacesOnTeam(liverpool);
+        System.out.println(spacesInTeamBefore);
+
+        teamService.addPlayerToTeam(liverpool, salah);
+
+        int spacesInTeamAfter = teamService.countEmptySpacesOnTeam(liverpool);
+        System.out.println(spacesInTeamAfter);
     }
 }
--- src/main/java/teams/TeamService.java
+++ src/main/java/teams/TeamService.java
@@ -22,4 +22,25 @@ public class TeamService {
         }
         return count;
     }
+
+
+    public void addPlayerToTeam(Team team, Player player) {
+        // Check if there is space on the team
+        int spaces = countEmptySpacesOnTeam(team);
+        // if there is, add the player
+        if (spaces > 0) {
+            // get players array from team
+            Player[] players = team.getPlayers();
+
+            // add player to first available empty space
+            // loop through players array
+            for (int i = 0; i < players.length; i++) {
+                // if we find an empty space, add the player then break
+                if (players[i] == null) {
+                    players[i] = player;
+                    break;
+                }
+            }
+        }
+    }
 }
